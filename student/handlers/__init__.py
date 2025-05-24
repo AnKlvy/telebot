139502d746a48575d0f7589e55946a4828cef295
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-05-24 20:23:49 +0500

    fix main menu transitions in student

--- student/handlers/__init__.py
+++ student/handlers/__init__.py
@@ -1,3 +1,4 @@
+from .main import router as main_router
 from .homework import router as homework_router
 from .progress import router as progress_router
 from .shop import router as shop_router
@@ -6,7 +7,8 @@ from .trial_ent import router as trial_ent_router
 from .curator_contact import router as curator_contact_router
 from .account import router as account_router
 
-router = homework_router
+router= main_router
+router.include_router(homework_router)
 router.include_router(progress_router)
 router.include_router(shop_router)
 router.include_router(test_report_router)
--- student/handlers/main.py
+++ student/handlers/main.py
@@ -27,7 +27,7 @@ async def show_student_main_menu(message: Message):
         reply_markup=get_student_main_menu_kb()
     )
 
-@router.callback_query(F.data == "back_to_main_menu")
+@router.callback_query(F.data == "back_to_main")
 async def back_to_main_menu(callback: CallbackQuery, state: FSMContext):
     """Возврат в главное меню"""
     await callback.message.delete()
