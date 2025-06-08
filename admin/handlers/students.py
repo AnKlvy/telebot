ac8ae5c43c6a4137cf5cc4259272a2395b95ff0a
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-06-09 01:52:21 +0500

    temp fix for courses import error

--- admin/handlers/students.py
+++ admin/handlers/students.py
@@ -5,11 +5,12 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import StateFilter
 
 from admin.utils.common import (
-    students_db, courses_db, groups_db,
+    students_db, groups_db,
     get_courses_list_kb, get_groups_list_kb, get_people_list_kb, 
     get_confirmation_kb, get_tariff_selection_kb, add_person, remove_person
 )
 from common.keyboards import get_home_kb
+from manager.handlers.lessons import courses_db
 
 router = Router()
 
